d61b007b8be8e1c5d0139d5948eca69ec71113cd
ParaHuang <paradox041@gmail.com>
2025-03-16 09:55:15 +0800

    day5

---
+++ AnnabelDemo/src/day5/Main1.java
@@ -0,0 +1,62 @@
+package day5;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		//python for loop
+		//for i in range(5):
+			// i-> 0,1,2,3,4
+		
+		//loop
+		//1.while
+		/*
+		while(statement) {
+			operation
+		}
+		if the statement is true, keep executing the operation part until the statement is false
+		*/
+		//2.for
+		//3.do...while
+		
+//		if(1<5) {
+//			System.out.println("hello");
+//		}
+		
+//		situation 1: loop didn't happen
+//		int i = 1;
+//		while(i>5) {
+//			System.out.println("hello");
+//		}
+
+//		situation 2: endless loop
+//		while(1<5) {
+//			System.out.println("hello");
+//		}
+		
+		
+//		situation 3:limit times of loop
+		//print hello for 5 times
+		int i=1;
+//		System.out.println(i<5);
+//		System.out.println(i==5);
+//		System.out.println(i<=5);//true or false
+		while(i<=5) {
+			System.out.println("hello");
+			//increase i
+			i = i+1;//2
+		}
+		
+
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+//		System.out.println("hello");
+	}
+
+}
